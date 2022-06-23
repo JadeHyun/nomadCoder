3ffc95c71c4b9022b33a8b9a602043fb362759b7
Hyunjun <yhj0439@nate.com>
2022-06-24 02:14:46 +0900

    react script 추가방식에서 CDN 오류로 create-react-app으로 대체

---
+++ movie-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello i'm create by React DOM</h3>
+      <button>Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
