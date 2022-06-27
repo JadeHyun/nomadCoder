915eadb52d33aff193166fd23df6b6e00c1ff655
Hyunjun <yhj0439@nate.com>
2022-06-28 02:05:03 +0900

    toDo list 만들기 연습

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -17,6 +17,21 @@ function App() {
   const [keyword, setkeyword] = useState("")
   const onClick = ()=>{ setcounter( a => a+1) }
   const [show, setshow] = useState(false)
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+
+  const onvalue = (e)=>{
+    setToDo(e.target.value)
+  }
+  const onSubmit = (e)=>{
+    e.preventDefault()
+    if(toDo === ""){
+      return;
+    }
+    setToDos(a => [toDo, ...a])
+    setToDo("")
+  }
+  console.log(toDos);
 
   const onclick = (e)=>{
     setshow( a => !a) // 클릭했을 경우 다시 전의 상태로 돌아가고 싶으면 부정연산자(!)를 쓰면 되겠구나!
@@ -26,17 +41,26 @@ function App() {
     setkeyword(e.target.value)
   }
   const iRunOnly = () =>{
-    console.log('i run only once')
+    // console.log('i run only once')
   }
 
   useEffect(iRunOnly,[])
   useEffect(()=>{
     if(keyword.length > 5){
-      console.log('keyword working!!')
+      // console.log('keyword working!!')
     }
   },[keyword])
   return (
     <div className="App">
+      <h1>My ToDos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={toDo} placeholder='write yout to do...' onChange={onvalue}></input>
+        <button>Add to do</button>
+        <hr/>
+        <ul>
+          {toDos.map((el,index) => <li key={index}>{el}</li>)}
+        </ul>
+      </form>
       <button onClick={onclick}>{show ? "hide" : "show"}</button>
       {show ? <Hello /> : null}
       <input value={keyword} type="text" placeholder='Search here' onChange={onChange}></input>
