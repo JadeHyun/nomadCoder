96e43730c06b76a8e277d07f99f9a4f0e84734d9
Hyunjun <yhj0439@nate.com>
2022-07-01 00:29:46 +0900

    useStates & useInput

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,12 +1,14 @@
 import Home from './routes/Home';
 import {BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Detail from './routes/Detail';
+import styled from "styled-components"
+
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/movie/:id' element={<Detail/>}></Route>
+      {/* <Route path='/movie/:id' element={<Detail/>}></Route> */}
       <Route path='/' element={<Home/>}></Route>
     </Routes>
     </BrowserRouter>
